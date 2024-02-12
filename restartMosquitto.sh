3c27d179bc3f0b731d4fb44420e240cd0e36f295
James Colton <james.colton@issacorp.com>
2024-02-12 15:37:27 -0700

    make path absolute

--- restartMosquitto.sh
+++ restartMosquitto.sh
@@ -2,7 +2,10 @@
 #
 #
 containerName=mqtt
+pullImageName='eclipse-mosquitto:2.0.18'
+imageName=192.168.1.99:5000/component/$pullImageName
+
 docker stop $containerName
 docker rm $containerName
 
-docker run --restart always --name $containerName -d -p 1883:1883 -p 9001:9001 -v ./mosquitto.conf:/mosquitto/config/mosquitto.conf eclipse-mosquitto  
+docker run --restart always --name $containerName -d -p 1883:1883 -p 9001:9001 -v $(pwd)/mosquitto.conf:/mosquitto/config/mosquitto.conf $imageName
