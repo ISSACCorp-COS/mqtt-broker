28d94914ce70e1aa4eef0032801d43cf4ed00396
James Colton <james.colton@issacorp.com>
2024-02-12 15:43:11 -0700

    formatted the script

--- restartMosquitto.sh
+++ restartMosquitto.sh
@@ -8,4 +8,12 @@ imageName=192.168.1.99:5000/component/$pullImageName
 docker stop $containerName
 docker rm $containerName
 
-docker run --restart always --name $containerName -d -p 1883:1883 -p 9001:9001 -v $(pwd)/mosquitto.conf:/mosquitto/config/mosquitto.conf $imageName
+docker run \
+	--restart always \
+	--name $containerName \
+	-d \
+	-p 1883:1883 \
+  	-p 9001:9001 \
+	-v $(pwd)/mosquitto.conf:/mosquitto/config/mosquitto.conf \
+	--privileged \
+	$imageName
